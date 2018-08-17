e4911a1fca82c32cc54e9dbc73aca7fbd00d8304
Joe Abbate <joseph.abbate7@gmail.com>
2018-08-17 14:02:25 -0700

    finish up access index CSVs and creation

--- RShiny/get_data.R
+++ RShiny/get_data.R
@@ -34,20 +34,30 @@ aggregate_session_nbhds = dbGetQuery(con, "SELECT * from shiny.aggregate_program
 
 # Aggregated DPS student data for demographics
 # aggregate_dps_student_nbhds = dbGetQuery(con, "SELECT * from shiny.dps_student_aggregate_nbhd")
-aggregate_dps_student_nbhds = read.csv("../data/aggregate_dps_student_nbhds.csv",check.names=FALSE)
+aggregate_dps_student_nbhds = read.csv("../data/shiny_tables/aggregate_dps_student_nbhds.csv",check.names=FALSE)
 
 # Search data from Google Analytics
 google_analytics = dbGetQuery(con, "SELECT * from clean.google_analytics")
 
 # Access index stuff
-driving_index = dbGetQuery(con, "SELECT * from clean.driving_index")
-driving_index_disability = dbGetQuery(con, "SELECT * from clean.driving_index_disability")
-driving_index_nbhd = dbGetQuery(con, "SELECT * from clean.driving_index_nbhd")
-driving_index_disability_nbhd = dbGetQuery(con, "SELECT * from clean.driving_index_disability_nbhd")
-transit_index = dbGetQuery(con, "SELECT * from clean.transit_index")
-transit_index_disability = dbGetQuery(con, "SELECT * from clean.transit_index_disability")
-transit_index_nbhd = dbGetQuery(con, "SELECT * from clean.transit_index_nbhd")
-transit_index_disability_nbhd = dbGetQuery(con, "SELECT * from clean.transit_index_disability_nbhd")
+
+driving_index <- read.csv("../data/shiny_tables/access_indices/driving_index.csv")
+driving_index_disability <- read.csv("../data/shiny_tables/access_indices/driving_index_disability.csv")
+transit_index <- read.csv("../data/shiny_tables/access_indices/transit_index.csv")
+transit_index_disability <- read.csv("../data/shiny_tables/access_indices/transit_index_disability.csv")
+driving_index_nbhd <- read.csv("../data/shiny_tables/access_indices/driving_index_nbhd.csv")
+driving_index_disability_nbhd <- read.csv("../data/shiny_tables/access_indices/driving_index_disability_nbhd.csv")
+transit_index_nbhd <- read.csv("../data/shiny_tables/access_indices/transit_index_nbhd.csv")
+transit_index_nbhd_disability_nbhd <- read.csv("../data/shiny_tables/access_indices/transit_index_disability_nbhd.csv")
+
+# driving_index = dbGetQuery(con, "SELECT * from clean.driving_index")
+# driving_index_disability = dbGetQuery(con, "SELECT * from clean.driving_index_disability")
+# driving_index_nbhd = dbGetQuery(con, "SELECT * from clean.driving_index_nbhd")
+# driving_index_disability_nbhd = dbGetQuery(con, "SELECT * from clean.driving_index_disability_nbhd")
+# transit_index = dbGetQuery(con, "SELECT * from clean.transit_index")
+# transit_index_disability = dbGetQuery(con, "SELECT * from clean.transit_index_disability")
+# transit_index_nbhd = dbGetQuery(con, "SELECT * from clean.transit_index_nbhd")
+# transit_index_disability_nbhd = dbGetQuery(con, "SELECT * from clean.transit_index_disability_nbhd")
 
 # when you're done, close the connection and unload the driver 
 dbDisconnect(con) 
